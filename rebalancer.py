21c57391c6ab97349a9134053ed7c639a5d62fd4
BhaagBodeDK <vaibhav.cbrain@gmail.com>
2022-07-18 22:11:07 +0000

    Rebalance fee information in alias (hack)

--- rebalancer.py
+++ rebalancer.py
@@ -42,8 +42,6 @@ def run_rebalancer(rebalance):
         rebalance.save()
         for payment_response in routerstub.SendPaymentV2(lnr.SendPaymentRequest(payment_request=str(invoice_response.payment_request), fee_limit_msat=int(rebalance.fee_limit*1000), outgoing_chan_ids=chan_ids, last_hop_pubkey=bytes.fromhex(rebalance.last_hop_pubkey), timeout_seconds=(timeout-5), allow_self_payment=True, no_inflight_updates=True), timeout=(timeout+60)):
             print ('Payment Response:', payment_response.status, ' Fee: ', payment_response.fee_msat/1000, ' Reason:', payment_response.failure_reason, 'Payment Hash :', payment_response.payment_hash )
-            original_alias = rebalance.target_alias
-            rebalance.target_alias += ' ==>fee paid : ' + str(payment_response.fee_msat/1000)
             rebalance.payment_hash = payment_response.payment_hash
             if payment_response.status == 1 and rebalance.status == 0:
                 #IN-FLIGHT
@@ -82,8 +80,12 @@ def run_rebalancer(rebalance):
             print(error)
     finally:
         rebalance.stop = datetime.now()
-        rebalance.save()
-        if rebalance.status == 2:
+
+        if rebalance.status ==2:
+
+            original_alias = rebalance.target_alias
+            rebalance.target_alias += ' ==>fee paid : ' + str(payment_response.fee_msat/1000)
+
             update_channels(stub, rebalance.last_hop_pubkey, successful_out)
             auto_rebalance_channels = Channels.objects.filter(is_active=True, is_open=True, private=False).annotate(percent_outbound=((Sum('local_balance')+Sum('pending_outbound'))*100)/Sum('capacity')).annotate(inbound_can=(((Sum('remote_balance')+Sum('pending_inbound'))*100)/Sum('capacity'))/Sum('ar_in_target'))
             inbound_cans = auto_rebalance_channels.filter(remote_pubkey=rebalance.last_hop_pubkey).filter(auto_rebalance=True, inbound_can__gte=1)
@@ -95,6 +97,9 @@ def run_rebalancer(rebalance):
                 next_rebalance = None
         else:
             next_rebalance = None
+
+        rebalance.save()
+
         return next_rebalance
 
 def update_channels(stub, incoming_channel, outgoing_channel):
